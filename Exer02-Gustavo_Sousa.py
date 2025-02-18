8e96c145a9b8acd92f46ceaef06af8232ae7f67e
GustavoSSousaa <gustasantossousa2008@gmail.com>
2025-02-17 21:28:20 -0300

    .

---
+++ Exer02-Gustavo_Sousa.py
@@ -0,0 +1,6 @@
+nome = str(input("Digite seu nome : "))
+peso = float(input("Digite seu peso : "))
+if (peso < 52):
+    print ("Categoria invalida ")
+elif (peso >=52 and <65):
+    print ("Peso pena")
